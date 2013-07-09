5be4dec175c9e672ec5e7e011be604ad39459565
Hoat Le <hoatlevan@gmail.com>
2013-07-09 16:31:38 +0000

    Add date_hierarchy = 'pub_date' to PollAdmin

--- apps/polls/admin.py
+++ apps/polls/admin.py
@@ -15,6 +15,7 @@ class PollAdmin(admin.ModelAdmin):
     list_display = ('question', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question']
+    date_hierarchy = 'pub_date'
 
 
 admin.site.register(Poll, PollAdmin)
