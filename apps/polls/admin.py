2041df41c2a83de4e885d4d88cc16be5d57ed276
Hoat Le <hoatlevan@gmail.com>
2013-07-09 16:14:33 +0000

    Add fieldsets

--- apps/polls/admin.py
+++ apps/polls/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from apps.polls.models import Poll
 
 class PollAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question']
+    fieldsets = [
+        (None, {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
 
 admin.site.register(Poll, PollAdmin)
