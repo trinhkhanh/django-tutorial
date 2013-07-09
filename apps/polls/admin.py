f6de10ede1add791bfbf2c7732a9314986532a03
Hoat Le <hoatlevan@gmail.com>
2013-07-09 16:20:30 +0000

    Add ChoiceInline

--- apps/polls/admin.py
+++ apps/polls/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 
 from apps.polls.models import Poll, Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Poll, PollAdmin)
