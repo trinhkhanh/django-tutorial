4558b6c884c62b2ea60d528b27b2f52f41d99ae0
Hoat Le <hoatlevan@gmail.com>
2013-07-09 17:55:33 +0000

    Namespacing URL names

--- urls/dev.py
+++ urls/dev.py
@@ -16,5 +16,5 @@ urlpatterns += staticfiles_urlpatterns()
 
 urlpatterns += (
     url(r'', include('apps.hello.urls')),
-    url(r'^polls/', include('apps.polls.urls')),
+    url(r'^polls/', include('apps.polls.urls', namespace='polls')),
 )
