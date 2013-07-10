ac1ccf0013ee74b5455f87fc782e9f04655c0c30
Hoat Le <hoatlevan@gmail.com>
2013-07-10 01:02:57 +0000

    Testing our new view

--- apps/polls/tests.py
+++ apps/polls/tests.py
@@ -1,9 +1,19 @@
 import datetime
 
+from django.core.urlresolvers import reverse
 from django.utils import timezone
 from django.test import TestCase
 
 from apps.polls.models import Poll
+
+def create_poll(question, days):
+    """
+    Creates a poll with the given `question` published the given number of
+    `days` offset to now (negative for polls published in the past,
+    positive for polls that have yet to be published).
+    """
+    return Poll.objects.create(question=question,
+        pub_date=timezone.now() + datetime.timedelta(days=days))
 
 
 class PollMethodTests(TestCase):
@@ -33,3 +43,58 @@ class PollMethodTests(TestCase):
         recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
         self.assertEqual(recent_poll.was_published_recently(), True)
 
+class PollViewTests(TestCase):
+
+    def test_index_view_with_no_polls(self):
+        """
+        If no polls exists, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_poll_list'], [])
+
+    def test_index_view_with_a_pass_poll(self):
+        """
+        Polls with a pub_date in the past should be displayed on the index page
+        """
+        create_poll(question="Past poll.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_poll_list'], ['<Poll: Past poll.>'])
+
+
+    def test_index_view_with_a_future_poll(self):
+        """
+        Polls with a pub_date ini the future should not be displayed on the index page.
+        """
+        create_poll(question="Future poll", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_poll_list'], [])
+
+
+    def test_index_view_with_future_and_pass_poll(self):
+        """
+        Event if both past and future polls exist, only past polls should
+        be displayed only.
+        """
+        create_poll(question="Past poll.", days=-30)
+        create_poll(question="Future poll.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "Past poll.")
+        self.assertQuerysetEqual(
+            response.context['latest_poll_list'],
+            ['<Poll: Past poll.>']
+        )
+
+    def test_index_view_with_two_past_polls(self):
+        """
+        The poll index page may display multiple polls.
+        """
+        create_poll(question="Past poll 1.", days=-30)
+        create_poll(question="Past poll 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_poll_list'],
+            ['<Poll: Past poll 2.>', '<Poll: Past poll 1.>']
+        )
--- apps/polls/views.py
+++ apps/polls/views.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from django.views import generic
 
 from apps.polls.models import Poll, Choice
@@ -11,8 +12,12 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_poll_list'
 
     def get_queryset(self):
-        """Return the last five published polls."""
-        return Poll.objects.order_by('-pub_date')[:5]
+        """Return the last five published polls (not including those set to be
+           published in the future).
+        """
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
